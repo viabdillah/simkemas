15a536646f44176bd0cb5d138cb837c9c906d46e
Divi Abdillah Almasrur <diviabdillah94@gmail.com>
2026-07-31 02:33:58 +0700

    feat: initial commit - setup monorepo, auth & cloudflare d1

--- frontend/functions/api/[[path]].js
+++ frontend/functions/api/[[path]].js
@@ -1,17 +1,15 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
-  const backendUrl = new URL("https://e8dee652.simkemas-api.pages.dev/" + url.pathname + url.search);
+  const backendUrl = new URL("https://simkemas-api.pages.dev" + url.pathname + url.search);
   const newRequest = new Request(backendUrl.toString(), context.request);
-  
+
   try {
     return await fetch(newRequest);
   } catch (error) {
-    // FIX ESLINT: Gunakan variabel error untuk debugging log di server
     console.error("Smart Proxy Error:", error);
-    
-    return new Response(JSON.stringify({ 
-      ok: false, 
-      error: "Smart Proxy Gagal menyambung ke Backend" 
+    return new Response(JSON.stringify({
+      ok: false,
+      error: "Smart Proxy Gagal menyambung ke Backend"
     }), { status: 502 });
   }
 }
